4196356211a72589776f382749f5f8d3074ae07e
Ian Leyden <thefreebird777@gmail.com>
2016-04-28 04:52:59 -0500

    updated version

--- app/src/main/java/it226/myapplicationit226androidapp/Logic.java
+++ app/src/main/java/it226/myapplicationit226androidapp/Logic.java
@@ -1,19 +1,15 @@
 package it226.myapplicationit226androidapp;
 
-import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.util.Log;
-import android.widget.Toast;
+
 
 public class Logic extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.e("test","test");
-            Toast.makeText(context, messageActivity.getMes()+"\n Lat: "+messageActivity.getLat()+"\n Long: "+ messageActivity.getLon(),
-                    Toast.LENGTH_LONG).show();
+        Intent newIntent = new Intent(context, Displayer.class);
+                newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(newIntent);
     }
-
 }
